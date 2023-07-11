cd50125212bb107a976355faaccc3f940e6811b4
Ditidey <ditidey2017@gmail.com>
2023-07-11 16:34:44 +0100

    installation, express, apis, web socket added

---
+++ index.js
@@ -0,0 +1,79 @@
+const express = require('express');
+const WebSocket = require('ws');
+const cors = require('cors');
+require('dotenv').config();
+// const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+
+app.use(cors());
+app.use(express.json())
+
+const wss = new WebSocket.Server({ server });
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.kos6m2u.mongodb.net/?retryWrites=true&w=majority`;
+ const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try { 
+    // await client.connect();
+    const usersCollection = client.db('blood-care').collection('users');
+    
+    app.post('/users', async(req, res)=>{
+      const userInfo = req.body;
+      const result = await usersCollection.insertOne(userInfo);
+      res.send(result);
+    })
+    app.get('/users', async(req, res)=>{
+        const result = await usersCollection.find().toArray();
+        res.send(result)
+    })
+
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res)=>{
+    res.send('Blood care is exchanging')
+})
+// app.listen(port, ()=>{
+//     console.log(`blood care server running on ${port}`)
+// }) 
+
+// web socket 
+// const wss = new WebSocket.Server({ server });
+
+wss.on('connection', (ws) => {
+  // console.log(ws)
+  // console.log('WebSocket client connected.');
+  //  ws.send('Welcome new user!')
+  ws.on('message', (message) => {
+    console.log('Received message:', message);
+    ws.send('' + message)
+
+    // Send a response back to the WebSocket client
+    // ws.send('send');
+  });
+
+  ws.on('close', () => {
+    console.log('WebSocket client disconnected.');
+  });
+});
+ 
+server.listen(process.env.PORT || 5000, () => {
+  console.log(`Blood care server running on port ${process.env.PORT || 5000}`);
+});
